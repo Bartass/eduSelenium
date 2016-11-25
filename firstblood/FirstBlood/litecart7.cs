0448bf4654ce107b7d56d439202ac5d0c1165ed6
Bartass <bartass@mail.ru>
2016-11-25 15:11:38 +0300

    Tasks7-8

---
+++ firstblood/FirstBlood/litecart7.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+
+namespace Litecart
+{
+    [TestFixture]
+    public class litecart7
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver("C:\\1vs\\FirstBlood");
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void litecart7task()
+        {
+            //login
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.CssSelector(".input-wrapper [name='username']")).SendKeys("admin");
+            driver.FindElement(By.CssSelector(".input-wrapper [name='password']")).SendKeys("admin");
+            driver.FindElement(By.CssSelector(".footer [name='login']")).Click();
+            //menu
+            int count = driver.FindElements(By.CssSelector("#box-apps-menu li#app-")).Count;
+            for (int i = 1; i <= count; i++)
+            {
+                var level1 = "#box-apps-menu li:nth-child("+ i.ToString() +")";
+                driver.FindElement(By.CssSelector(level1)).Click();
+                for (int j = 1; ; j++)
+                {
+                    var level2 = level1 + " li:nth-child("+ j.ToString() +")";
+                    try
+                    {
+                        driver.FindElement(By.CssSelector(level2)).Click();
+                    }
+                    catch(NoSuchElementException) {
+                        break; }
+                    driver.FindElement(By.CssSelector("#content > h1"));
+                }
+            }
+        }
+
+
+        //[TearDown]
+        //public void stop()
+        //{
+        //    driver.Quit();
+        //    driver = null;
+        //}
+    }
+}
---
+++ firstblood/FirstBlood/litecart8.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+
+
+namespace Litecart
+{
+    [TestFixture]
+    public class litecart8
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver("C:\\1vs\\FirstBlood");
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void litecart8task()
+        {            
+            //Navigate
+            driver.Url = "http://localhost/litecart/en/";
+            //CheckStickers
+            int count1 = driver.FindElements(By.CssSelector("#box-most-popular li")).Count;
+            int count2 = driver.FindElements(By.CssSelector("#box-campaigns li")).Count;
+            int count3 = driver.FindElements(By.CssSelector("#box-latest-products li")).Count;
+
+            for (int i = 1; i <= count1; i++)
+            {
+                var duck = "#box-most-popular li:nth-child(" + i.ToString() + ") .sticker";
+                int stick = driver.FindElements(By.CssSelector(duck)).Count;
+                Assert.AreEqual(1, stick, "Стикера нет или их несколько :)");           
+            }
+            for (int j = 1; j <= count2; j++)
+            {
+                var duck = "#box-campaigns li:nth-child(" + j.ToString() + ") .sticker";
+                int stick = driver.FindElements(By.CssSelector(duck)).Count;
+                Assert.AreEqual(1, stick, "Стикера нет или их несколько :)");
+            }
+            for (int k = 1; k <= count3; k++)
+            {
+                var duck = "#box-latest-products li:nth-child(" + k.ToString() + ") .sticker";
+                int stick = driver.FindElements(By.CssSelector(duck)).Count;
+                Assert.AreEqual(1, stick, "Стикера нет или их несколько :)");
+            }
+        }
+
+        //[TearDown]
+        //public void stop()
+        //{
+        //    driver.Quit();
+        //    driver = null;
+        //}
+    }
+}
