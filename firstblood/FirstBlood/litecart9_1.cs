13e5ad937890520900df88fd631b1a60ebf9fa8e
Bartass <bartass@mail.ru>
2016-11-29 11:06:31 +0300

    tasks9

---
+++ firstblood/FirstBlood/litecart9_1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Linq;
+
+
+namespace Litecart
+{
+    [TestFixture]
+    public class litecart9_1
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver("C:\\1vs\\FirstBlood");
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void litecart9_1task()
+        {
+            //login
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.CssSelector(".input-wrapper [name='username']")).SendKeys("admin");
+            driver.FindElement(By.CssSelector(".input-wrapper [name='password']")).SendKeys("admin");
+            driver.FindElement(By.CssSelector(".footer [name='login']")).Click();
+            //Navigate
+            driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
+
+            //SortCountries
+            int count = driver.FindElements(By.CssSelector("form .row")).Count;
+            bool issort = true;
+            for (int i = 3; i <= count + 1; i++)
+            {
+                var country = ".row:nth-child(" + (i-1).ToString() + ") a";
+                string Predname = driver.FindElement(By.CssSelector(country)).Text;
+                country = ".row:nth-child(" + i.ToString() + ") a";
+                string Postname = driver.FindElement(By.CssSelector(country)).Text;
+                if (string.Compare(Predname,Postname,true) >= 0)
+                { issort = false; break; } 
+            }
+
+            //SortZones
+            List<int> links = new List<int>();
+            List<int> states = new List<int>();
+            int count2 = driver.FindElements(By.CssSelector("form .row")).Count;            
+            for (int i = 2; i <= count2 +1; i++)
+            {
+                var country2 = ".row:nth-child(" + i.ToString() + ") td:nth-child(6)";
+                string havezone = driver.FindElement(By.CssSelector(country2)).Text;
+                int k = Convert.ToInt32(havezone);
+                if (havezone != "0")
+                { links.Add(i); states.Add(k); }
+            }
+            for (int k = 0; k <= states.Count-1; k++)
+            {
+                driver.FindElement(By.CssSelector(".row:nth-child(" + links[k].ToString() + ") a")).Click();
+                for (int j = 1; j < states[k]; j++)
+                {
+                    var zone = "table#table-zones tr:nth-child(" + (j+1).ToString() + ") td:nth-child(3)";
+                    string Predzone = driver.FindElement(By.CssSelector(zone)).Text;
+                    zone = "table#table-zones tr:nth-child(" + (j+2).ToString() + ") td:nth-child(3)";
+                    string Postzone = driver.FindElement(By.CssSelector(zone)).Text;
+                    if (string.Compare(Predzone, Postzone, true) >= 0)
+                    { issort = false; break; }                 
+                }
+                driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
+            }
+        }
+
+        //[TearDown]
+        //public void stop()
+        //{
+        //    driver.Quit();
+        //    driver = null;
+        //}
+    }
+}
---
+++ firstblood/FirstBlood/litecart9_2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace Litecart
+{
+    [TestFixture]
+    public class litecart9_2
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver("C:\\1vs\\FirstBlood");
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void litecart9_2task()
+        {
+            //login
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.CssSelector(".input-wrapper [name='username']")).SendKeys("admin");
+            driver.FindElement(By.CssSelector(".input-wrapper [name='password']")).SendKeys("admin");
+            driver.FindElement(By.CssSelector(".footer [name='login']")).Click();
+            //Navigate
+            driver.Url = "http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones";
+            //SortGeoZones
+            int countcount = driver.FindElements(By.CssSelector("form .row")).Count;
+            bool issort = true;
+            for (int i = 2; i <= countcount + 1; i++)
+            {
+                var geo = ".row:nth-child(" + i.ToString() + ") a";
+                driver.FindElement(By.CssSelector(geo)).Click();
+                int countzone = driver.FindElements(By.CssSelector("table#table-zones tr")).Count;
+                for (int j = 2; j < countzone - 1; j++)
+                {
+                    var zone = ".dataTable tr:nth-child(" + j.ToString() + ") td:nth-child(3) select";
+                    string predNameValue = driver.FindElement(By.CssSelector(zone)).GetAttribute("value");
+                    string Predname = driver.FindElement(By.CssSelector(zone + " option[value='" + predNameValue + "']")).Text;
+
+                    zone = ".dataTable tr:nth-child(" + (j+1).ToString() + ") td:nth-child(3) select";
+                    string PostNameValue = driver.FindElement(By.CssSelector(zone)).GetAttribute("value");
+                    string Postname = driver.FindElement(By.CssSelector(zone + " option[value='" + PostNameValue + "']")).Text;
+                    if (string.Compare(Predname, Postname, true) >= 0)
+                    { issort = false; break; }
+                }
+                driver.Url = "http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones";
+            }
+        }
+
+        //[TearDown]
+        //public void stop()
+        //{
+        //    driver.Quit();
+        //    driver = null;
+        //}
+    }
+}
